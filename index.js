f123e3a0a361dd5fb198329998cfff40743dd7ab
Stephen Mathieson <me@stephenmathieson.com>
2016-12-12 16:48:43 -0800

    Remove ES6 syntax (#3)

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const url = require('url')
+var url = require('url')
 
-const serialize = (obj) => {
-  const json = JSON.stringify(obj)
+function serialize (obj) {
+  var json = JSON.stringify(obj)
   return new Buffer(json).toString('base64')
 }
 
-const parse = (str) => {
-  const json = new Buffer(str, 'base64').toString('utf8')
+function parse (str) {
+  var json = new Buffer(str, 'base64').toString('utf8')
   return JSON.parse(json)
 }
 
-module.exports = (req, key) => {
+module.exports = function (req, key) {
   key = key || 'data'
 
-  const query = url.parse(req.url, true).query
+  var query = url.parse(req.url, true).query
   return parse(query[key])
 }
 
