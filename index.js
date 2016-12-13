6ec2d477cf780fdfff8f71d3973fd5c8391c66b5
Stephen Mathieson <me@stephenmathieson.com>
2016-12-12 16:41:34 -0800

    s/const/var/g

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const url = require('url')
+var url = require('url')
 
 function serialize (obj) {
-  const json = JSON.stringify(obj)
+  var json = JSON.stringify(obj)
   return new Buffer(json).toString('base64')
 }
 
 function parse (str) {
-  const json = new Buffer(str, 'base64').toString('utf8')
+  var json = new Buffer(str, 'base64').toString('utf8')
   return JSON.parse(json)
 }
 
 module.exports = function (req, key) {
   key = key || 'data'
 
-  const query = url.parse(req.url, true).query
+  var query = url.parse(req.url, true).query
   return parse(query[key])
 }
 
